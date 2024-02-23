8691670a936d6b2115e4cc9771314b8f180e2015
gurma <gurma@10.48.3.191>
2024-02-23 10:17:52 -0500

    code modified

--- src/days/Day.java
+++ src/days/Day.java
@@ -6,30 +6,39 @@ package days;
 
 import java.util.Scanner;
 
-/**This class takes String input from user 
- * and calls method  to print the 
+/**
+ * This class takes String input from user
+ * and calls method to print the
  * name of the day.
- * if the input is not given in String, 
- * program will accept and produce wrong results. 
- * change the code to use enums which avoids String input 
- * then print the week day names. 
+ * if the input is not given in String,
+ * program will accept and produce wrong results.
+ * change the code to use enums which avoids String input
+ * then print the week day names.
  *
  * @author sivagamasrinivasan
- jn 22nd
+ *         jn 22nd
  */
 public class Days {
+    public enum Day {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the day number (one,two,three,four,five) in string");
-    String code = in.next();
-    Weekdays t= new Weekdays();
-    t.nameOfDay(code); 
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the day number (one,two,three,four,five) in string");
+        String code = in.next();
+        Weekdays t = new Weekdays();
+        t.nameOfDay(code);
     }// TODO code application logic here
-    
-    
+
 }
